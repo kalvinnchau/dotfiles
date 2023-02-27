0c0f8c68ebd99601283996dbcf048db5d7549a2c
kalvinnchau <kalvinnchau@gmail.com>
2023-02-26 20:31:36 -0800

    feat: add back trouble.nvim

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -245,6 +245,19 @@ return {
     },
   },
 
+  -- better diagnostics list and others
+  {
+    'folke/trouble.nvim',
+    cmd = { 'TroubleToggle', 'Trouble' },
+    opts = { use_diagnostic_signs = true },
+    keys = {
+      { '<leader>xx', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Document Diagnostics (Trouble)' },
+      { '<leader>xX', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Workspace Diagnostics (Trouble)' },
+      { '<leader>xL', '<cmd>TroubleToggle loclist<cr>', desc = 'Location List (Trouble)' },
+      { '<leader>xQ', '<cmd>TroubleToggle quickfix<cr>', desc = 'Quickfix List (Trouble)' },
+    },
+  },
+
   -- todo comments
   {
     'folke/todo-comments.nvim',
@@ -268,7 +281,7 @@ return {
         desc = 'Previous todo comment',
       },
       { '<leader>xt', '<cmd>TodoTrouble<cr>', desc = 'Todo Trouble' },
-      { '<leader>xtt', '<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>', desc = 'Todo Trouble' },
+      --{ '<leader>xtt', '<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>', desc = 'Todo Trouble' },
       { '<leader>xT', '<cmd>TodoTelescope<cr>', desc = 'Todo Telescope' },
     },
   },
