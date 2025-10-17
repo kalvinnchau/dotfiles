a3f6825f9b0c2e3ff96c0bc853b6f0f9fdd407a2
Kalvin Chau <kalvin@block.xyz>
2025-10-17 14:55:26 -0700

    feat: add snacks.nvim for better vim.ui (replace dressing.nvim)

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -149,6 +149,20 @@ return {
     end,
   },
 
+  -- better vim.ui
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = {
+      input = { enabled = true },
+      picker = {
+        enabled = true,
+        ui_select = true,
+      },
+    },
+  },
+
   -- ui components library
   'MunifTanjim/nui.nvim',
 }
